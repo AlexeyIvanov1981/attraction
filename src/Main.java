3e0944ecd51eec767788939bab4a16f9a5449403
AlexeyIvanov1981 <lelikus81@yandex.ru>
2022-11-29 20:24:42 +0300

    reformate code

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
+
         Queue<Person> attraction = new LinkedList<>();
         attraction.addAll(generateClients());
 
-        for(Person p : attraction){
-            System.out.println(p.getName());
+        for (Person p : attraction) {
+            System.out.println(p.getName() + " " + p.getSurname() + ", количество билетов - " + p.numberOfTickets);
         }
-
-        //System.out.println(attraction);
-
     }
 
     public static LinkedList<Person> generateClients() {
@@ -20,9 +17,9 @@ public class Main {
         LinkedList<Person> person = new LinkedList<>();
 
         person.add(new Person("Alex", "Do", 5));
-        person.add(new Person("Bob", "Mo", 3));
-        person.add(new Person("Tom", "Lo", 2));
-        person.add(new Person("Jack", "Ko", 4));
+        person.add(new Person("Bob", "Moon", 3));
+        person.add(new Person("Tom", "Lee", 2));
+        person.add(new Person("Jack", "Kovalsky", 4));
         person.add(new Person("Max", "Woo", 1));
 
 
